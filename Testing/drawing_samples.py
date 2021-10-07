b8aa6f2ba437a78ed12ae4474fff461d3004fbbe
Jake Sheeder <jake.sheeder@my.simpson.edu>
2021-10-07 12:37:57 -0500

    Work on lab 5

--- Testing/drawing_samples.py
+++ Testing/drawing_samples.py
@@ -1,18 +1,13 @@
-# function called count_up that takes in two numbers
-# prints all numbers from start to finish inclusive
-# test with 5, 10
-
-start = 5
-end = 10
-
-def count_up(start, end):
-    for current_number in range (start, end + 1):
-        print(current_number)
-
-count_up(5, 10)
+my_grades = [65, 98, 100, 95, 92, 93]
+print(my_grades[0] + 35)
 
 
-import random
+for row in range(10):
 
-my_number = random.randrange(50)
-print(my_number)
+    for j in range(row):
+        print(" ", end=" ")
+
+    for j in range(10 - row):
+        print(j, end=" ")
+
+    print()
--- Testing/test.py
+++ Testing/test.py
@@ -1,4 +1,51 @@
-x = 1
-while x < 64:
-    print(x)
-    x = x * 2
+x = [10, 20]
+print(x[0])
+# computer starts counting at 0 so if we want to print 10, we have to start at 0. 20 would be 1
+
+x = [3, 8, 7, 0, 5, 5, 2, 1]
+print(x[1])
+
+my_list = [3, 7, 3]
+
+for item in my_list:
+    print(item)
+
+
+my_list = [2, 3, 4, 5, 6, 7]
+print(my_list)
+
+my_list.append(100)
+print(my_list)
+
+
+my_list = []
+
+for i in range(5):
+    user_input = int(input("Enter an integer:"))
+    my_list.append(user_input)
+
+print(my_list)
+
+
+plain_text = "This is a test. ABC abc"
+for c in plain_text:
+    x = ord(c)
+    print(x, end="")
+
+
+class character:
+    """
+    This is a video game character.
+    """
+    def __init__(self):
+        """ Create my character """
+        self.name = ""
+        self.outfit = ""
+
+
+def main():
+    home_address = Address()
+    home_address.name = "John Smith"
+
+
+main()
